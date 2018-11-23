146648308cc69e6e475db432753e60deb46c526b
Rodrigo <marks.rodrigo@gmail.com>
2018-11-23 11:16:16 -0200

    home

---
+++ javas.js
@@ -0,0 +1,107 @@
+var content = document.getElementById("model");
+var button = document.getElementById("show-more");
+
+var content1 = document.getElementById("model1");
+var button1 = document.getElementById("show-more1");
+
+var content2 = document.getElementById("model2");
+var button2 = document.getElementById("show-more2");
+
+var content3 = document.getElementById("model3");
+var button3 = document.getElementById("show-more3");
+
+var content4 = document.getElementById("model4");
+var button4 = document.getElementById("show-more4");
+
+
+var content5 = document.getElementById("model5");
+var button5 = document.getElementById("show-more5");
+
+
+button.onclick = function () {
+
+    if (content.className == "open") {
+        content.className = "";
+        button.innerHTML = "Ver Mais";
+
+    } else {
+        content.className = "open";
+        button.innerHTML = "Ver Menos";
+    }
+
+}
+
+button1.onclick = function () {
+
+    if (content1.className == "open") {
+        content1.className = "";
+        button1.innerHTML = "Ver Mais";
+
+    } else {
+        content1.className = "open";
+        button1.innerHTML = "Ver Menos";
+    }
+
+}
+
+button2.onclick = function () {
+
+    if (content2.className == "open") {
+        content2.className = "";
+        button2.innerHTML = "Ver Mais";
+
+    } else {
+        content2.className = "open";
+        button2.innerHTML = "Ver Menos";
+    }
+
+}
+
+button3.onclick = function () {
+
+    if (content3.className == "open") {
+        content3.className = "";
+        button3.innerHTML = "Ver Mais";
+
+    } else {
+        content3.className = "open";
+        button3.innerHTML = "Ver Menos";
+    }
+
+}
+
+button4.onclick = function () {
+
+    if (content4.className == "open") {
+        content4.className = "";
+        button4.innerHTML = "Ver Mais";
+
+    } else {
+        content4.className = "open";
+        button4.innerHTML = "Ver Menos";
+    }
+
+}
+
+button5.onclick = function () {
+
+    if (content5.className == "open") {
+        content5.className = "";
+        button5.innerHTML = "Ver Mais";
+
+    } else {
+        content5.className = "open";
+        button5.innerHTML = "Ver Menos";
+    }
+
+}
+
+
+
+
+
+
+
+
+
+;
