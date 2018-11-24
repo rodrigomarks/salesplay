c70bfc86266178b037f71d4a1cb246d6e24df546
Rodrigo <marks.rodrigo@gmail.com>
2018-11-24 17:02:49 -0200

    Update javas.js

--- javas.js
+++ javas.js
@@ -1,6 +1,3 @@
-var content = document.getElementById("model");
-var button = document.getElementById("show-more");
-
 var content1 = document.getElementById("model1");
 var button1 = document.getElementById("show-more1");
 
@@ -13,33 +10,21 @@ var button3 = document.getElementById("show-more3");
 var content4 = document.getElementById("model4");
 var button4 = document.getElementById("show-more4");
 
-
 var content5 = document.getElementById("model5");
 var button5 = document.getElementById("show-more5");
 
-
-button.onclick = function () {
-
-    if (content.className == "open") {
-        content.className = "";
-        button.innerHTML = "Ver Mais";
-
-    } else {
-        content.className = "open";
-        button.innerHTML = "Ver Menos";
-    }
-
-}
+var content6 = document.getElementById("model6");
+var button6 = document.getElementById("show-more6");
 
 button1.onclick = function () {
 
     if (content1.className == "open") {
         content1.className = "";
-        button1.innerHTML = "Ver Mais";
+        button1.innerHTML = "+info";
 
     } else {
         content1.className = "open";
-        button1.innerHTML = "Ver Menos";
+        button1.innerHTML = "-info";
     }
 
 }
@@ -48,11 +33,11 @@ button2.onclick = function () {
 
     if (content2.className == "open") {
         content2.className = "";
-        button2.innerHTML = "Mais";
+        button2.innerHTML = "+info";
 
     } else {
         content2.className = "open";
-        button2.innerHTML = "Menos";
+        button2.innerHTML = "-info";
     }
 
 }
@@ -61,11 +46,11 @@ button3.onclick = function () {
 
     if (content3.className == "open") {
         content3.className = "";
-        button3.innerHTML = "Mais";
+        button3.innerHTML = "+info";
 
     } else {
         content3.className = "open";
-        button3.innerHTML = "Menos";
+        button3.innerHTML = "-info";
     }
 
 }
@@ -74,11 +59,11 @@ button4.onclick = function () {
 
     if (content4.className == "open") {
         content4.className = "";
-        button4.innerHTML = "Mais";
+        button4.innerHTML = "+info";
 
     } else {
         content4.className = "open";
-        button4.innerHTML = "Menos";
+        button4.innerHTML = "-info";
     }
 
 }
@@ -87,21 +72,24 @@ button5.onclick = function () {
 
     if (content5.className == "open") {
         content5.className = "";
-        button5.innerHTML = "Mais";
+        button5.innerHTML = "+info";
 
     } else {
         content5.className = "open";
-        button5.innerHTML = "Menos";
+        button5.innerHTML = "-info";
     }
 
 }
 
+button6.onclick = function () {
 
+    if (content6.className == "open") {
+        content6.className = "";
+        button6.innerHTML = "+info";
 
+    } else {
+        content6.className = "open";
+        button6.innerHTML = "-info";
+    }
 
-
-
-
-
-
-;
+}
